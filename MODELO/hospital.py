5d182922153d050e05ed28219dff3d65cfabf322
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-05-16 04:53:42 -0500

    Update hospital.py

--- MODELO/hospital.py
+++ MODELO/hospital.py
@@ -1,3 +1,9 @@
 class Hospital():
+    hospitalList = []
     def __init__(self,hospitalName):
         self.hospitalName = hospitalName
+        self.doctors = []
+        Hospital.hospitalList.append(self)
+        
+    def addDoctor(self,doctor):
+        self.doctors.append(doctor)
