286b4900ac4594c1da5a7318a7f7ae6039b2a186
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-05-16 04:53:22 -0500

    Update crud.py

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,14 +1,30 @@
 from abc  import ABC,abstractmethod
 from MODELO.doctor import Doctor
+from MODELO.hospital import Hospital
 search = Doctor.searchByDNI
 class Crud(ABC):
     @abstractmethod
-    def create(self,**kwargs):
+    def createHospital(self,**kwargs):
+       return Hospital(kwargs['hospitalName'])
+    
+    @abstractmethod
+    def createDoctor(self,**kwargs):
        return Doctor(kwargs['doctorName'],kwargs['speciality'],kwargs['dni'])
     
     @abstractmethod
     def search(self,**kwargs):
-        if kwargs == None:
+        if kwargs is None:
             raise NotImplementedError
         else:
          return search(kwargs['dni'])
+        
+class DoctorCrud(Crud):
+   def createHospital(self,**kwargs):
+        return super().createHospital(**kwargs)
+   
+   def createDoctor(self,**kwargs):
+        return super().createDoctor(**kwargs)
+   
+   def seach(self,**kwargs):
+      return super().search(**kwargs)
+   
